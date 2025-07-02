75ab6bd3143024b703d9891e8544eedd1dafb79d
Emmanuel Ogale <ogaleemmanuel467@gmail.com>
2025-07-02 17:04:23 +0000

    Fix error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCnJo7RLOf_Ctl05Yaxy4hAOhaabzKcy54",
   authDomain: "guad-eyes.firebaseapp.com",
   projectId: "guad-eyes",
-  storageBucket: "guad-eyes.appspot.com",
+  storageBucket: "guad-eyes.firebasestorage.app",
   messagingSenderId: "402502331730",
   appId: "1:402502331730:web:8f08df58cd9e01a0e69e8b"
 };
